08b3d6fe6723fdcebcecc4bc1bebea416943ee7a
johnStuart00 <ownworkplace01@gmail.com>
2024-02-27 22:22:15 +0500

    270120242222

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:online_library/pages/library_main_page/presentation/library_main_page.dart';
+import 'package:online_library/pages/sing_up_page/presentation/sing_up_page.dart';
 import 'package:online_library/pages/welcome_page/presentation/welcome_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import 'pages/sing_in_page/presentation/login_page.dart';
+
 void main() {
-  runApp(const GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Online Library",
-    localizationsDelegates: AppLocalizations.localizationsDelegates,
-    supportedLocales: AppLocalizations.supportedLocales,
-    routes:{
-    '/': (context) => FirstScreen(),
-    '/second': (context) => SecondScreen(),
-  },
-    home: OnlineLibrary(),
-  ));
+  runApp(const OnlineLibrary());
 }
 
 class OnlineLibrary extends StatelessWidget {
@@ -22,6 +16,24 @@ class OnlineLibrary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const WelcomePage();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Online Library",
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      routes: {
+        '/welcome': (context) => WelcomePage(),
+        '/singup': (context) => SingUpPage(),
+        '/login': (context) => LogInPage(),
+        '/librarymain': (context) => LibraryMainPage(),
+      },
+      initialRoute: '/welcome',
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute<void>(builder: (context) {
+          return const Text('Default Route');
+        });
+      },
+      // home: WelcomePage(),
+    );
   }
 }
