ccdce12fd36e08bee64fe8dba2107668338447c7
johnStuart00 <ownworkplace01@gmail.com>
2024-02-27 21:49:29 +0500

    ikinji comment

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:online_library/pages/welcome_page/presentation/welcome_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:online_library/pages/library_main_page/presentation/library_main_page.dart';
-import 'package:online_library/pages/sing_up_page/presentation/login_page.dart';
-import 'package:online_library/pages/sing_up_page/presentation/sing_up_page.dart';
 
 void main() {
-  runApp(GetMaterialApp(
+  runApp(const GetMaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Online Library",
     localizationsDelegates: AppLocalizations.localizationsDelegates,
     supportedLocales: AppLocalizations.supportedLocales,
-    routes: {
-    '/welcome': (context) => WelcomePage(),
-    '/singup': (context) => SingUpPage(),
-    '/singin': (context) => LogInPage(),
-    '/librarymain': (context) => LibraryMainPage(),
-  },
-    initialRoute: '/librarymain',
+    home: OnlineLibrary(),
   ));
 }
 
