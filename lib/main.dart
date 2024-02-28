3aab6b038ce94d4df4f8597a55fd2ed7afd10ab7
johnStuart00 <ownworkplace01@gmail.com>
2024-02-28 22:42:35 +0500

    280220242242

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:online_library/pages/library_main_page/presentation/library_main_page.dart';
 import 'package:online_library/pages/sing_up_page/presentation/sing_up_page.dart';
 import 'package:online_library/pages/welcome_page/presentation/welcome_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 import 'pages/sing_in_page/presentation/login_page.dart';
 
 void main() {
@@ -21,18 +21,18 @@ class OnlineLibrary extends StatelessWidget {
       title: "Online Library",
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      routes: {
-        '/welcome': (context) => WelcomePage(),
-        '/singup': (context) => SingUpPage(),
-        '/login': (context) => LogInPage(),
-        '/librarymain': (context) => LibraryMainPage(),
-      },
-      initialRoute: '/librarymain',
-      onGenerateRoute: (RouteSettings settings) {
-        return MaterialPageRoute<void>(builder: (context) {
-          return const Text('Default Route');
-        });
-      },
+      initialRoute: '/libraryMain',
+      getPages: [
+        GetPage(name: '/welcome', page: () => WelcomePage()),
+        GetPage(name: '/ligInPage', page: () => LogInPage()),
+        GetPage(name: '/singUpPage', page: () => SingUpPage()),
+        GetPage(name: '/libraryMain', page: () => LibraryMainPage())
+      ],
+      // onGenerateRoute: (RouteSettings settings) {
+      //   return MaterialPageRoute<void>(builder: (context) {
+      //     return const Text('Default Route');
+      //   });
+      // },
       // home: WelcomePage(),
     );
   }
