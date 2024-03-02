98d48e9f4e5a0776b57bed2d0cb8ba5032838be9
johnStuart00 <ownworkplace01@gmail.com>
2024-03-02 12:28:01 +0500

    020320241227

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:online_library/pages/library_main_page/presentation/library_main_page.dart';
 import 'package:online_library/pages/sing_up_page/presentation/sing_up_page.dart';
@@ -16,18 +15,18 @@ class OnlineLibrary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Online Library",
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       initialRoute: '/libraryMain',
-      getPages: [
-        GetPage(name: '/welcome', page: () => WelcomePage()),
-        GetPage(name: '/ligInPage', page: () => LogInPage()),
-        GetPage(name: '/singUpPage', page: () => SingUpPage()),
-        GetPage(name: '/libraryMain', page: () => LibraryMainPage())
-      ],
+      routes: {
+        '/welcome': (context) => const WelcomePage(),
+        '/singIn': (context) => const LogInPage(),
+        'singUp': (context) => SingUpPage(),
+        '/libraryMain': (context) => const LibraryMainPage(),
+      },
       theme: ThemeData(
           primaryColor: Colors.white,
           bottomNavigationBarTheme: BottomNavigationBarThemeData(
