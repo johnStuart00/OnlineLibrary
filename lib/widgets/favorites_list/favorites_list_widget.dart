28ba45164501dfd747b4f42e306c42bf47a4dc88
Cyber Security 202318 <202318@tmhi.edu>
2024-03-06 23:16:24 +0500

    070320242316

--- lib/widgets/favorites_list/favorites_list_widget.dart
+++ lib/widgets/favorites_list/favorites_list_widget.dart
@@ -5,10 +5,26 @@ class FavoritesListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (BuildContext context, int index) {
-      Container(
-        color: Colors.yellow,
-      );
-    });
+    return ListView.builder(
+        itemCount: 10,
+        itemExtent: 163,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            child: Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black.withOpacity(0.2),
+                  ),
+                  borderRadius: const BorderRadius.all(Radius.circular(10))),
+              child: Row(
+                children: [
+                  Image.asset('assets/images/p_luca_21670_3c13c611.jpeg'),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
