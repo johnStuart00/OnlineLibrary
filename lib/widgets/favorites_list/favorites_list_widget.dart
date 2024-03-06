479fd89c5d543bd46d3fb683958123f021e2c4c7
Cyber Security 202318 <202318@tmhi.edu>
2024-03-07 00:06:57 +0500

    070320240006

--- lib/widgets/favorites_list/favorites_list_widget.dart
+++ lib/widgets/favorites_list/favorites_list_widget.dart
@@ -11,18 +11,64 @@ class FavoritesListWidget extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-            child: Container(
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black.withOpacity(0.2),
+            child: Stack(
+              children: [
+                Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black.withOpacity(0.2),
+                      ),
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 1,
+                          offset: const Offset(0, 2),
+                        ),
+                      ]),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/p_luca_21670_3c13c611.jpeg'),
+                      const Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Book name',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              'Book info',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              'Book description',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(height: 20),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                  borderRadius: const BorderRadius.all(Radius.circular(10))),
-              child: Row(
-                children: [
-                  Image.asset('assets/images/p_luca_21670_3c13c611.jpeg'),
-                ],
-              ),
+                ),
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () {
+                      print('1');
+                    },
+                  ),
+                )
+              ],
             ),
           );
         });
