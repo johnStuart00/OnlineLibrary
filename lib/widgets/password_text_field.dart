eb51aae08f6d58663e33d222cf3f7ca3b86f8133
Cyber Security 202318 <202318@tmhi.edu>
2024-03-07 22:03:25 +0500

    070320242203

--- lib/widgets/password_text_field.dart
+++ lib/widgets/password_text_field.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-// ignore: must_be_immutable
 class PasswordTextField extends StatefulWidget {
-  PasswordTextField({
+  const PasswordTextField({
     super.key,
     required this.passwordController,
   });
