9ae476b5e66280cb1ef7efb7f9981ee321a72f80
Cyber Security 202318 <202318@tmhi.edu>
2024-03-11 22:40:24 +0500

    '110320242205-changed'

--- lib/widgets/phone_number_text_field.dart
+++ lib/widgets/phone_number_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // ignore: must_be_immutable
 class PhoneNumberTextField extends StatelessWidget {
@@ -21,15 +22,40 @@ class PhoneNumberTextField extends StatelessWidget {
       width: 300,
       child: TextField(
         controller: userNameController,
+        keyboardType: TextInputType.phone,
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(9),
+          FilteringTextInputFormatter.digitsOnly,
+          _PhoneNumberFormatter(),
+        ],
         decoration: InputDecoration(
           border: const OutlineInputBorder(),
           labelText: label,
+          prefix: const Text('+993 '),
+          //hintText: '+993 -- -- -- -- --',
           suffix: GestureDetector(
             onTap: textFieldClear,
             child: const Icon(Icons.clear_rounded),
           ),
         ),
       ),
+    );
+  }
+}
+
+class _PhoneNumberFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    var newText = newValue.text;
+
+    if (newText.length > 2 && !newText.contains(' ')) {
+      newText = newText.substring(0, 2) + ' ' + newText.substring(2);
+    }
+
+    return newValue.copyWith(
+      text: newText,
+      selection: TextSelection.collapsed(offset: newText.length),
     );
   }
 }
