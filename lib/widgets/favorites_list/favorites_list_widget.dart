d5f8a95e06adb83f79e244738e0676d578ff4d54
Cyber Security 202318 <202318@tmhi.edu>
2024-03-12 20:45:46 +0500

    120320242326-craeted main details page

--- lib/widgets/favorites_list/favorites_list_widget.dart
+++ lib/widgets/favorites_list/favorites_list_widget.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Books {
+  final id;
   final String imageName;
   final String title;
   final String bookInfo;
   final String description;
 
-  Books(
-      {required this.imageName,
-      required this.title,
-      required this.bookInfo,
-      required this.description});
+  Books({
+    required this.id,
+    required this.imageName,
+    required this.title,
+    required this.bookInfo,
+    required this.description,
+  });
 }
 
 class FavoritesListWidget extends StatefulWidget {
@@ -24,30 +27,35 @@ class FavoritesListWidget extends StatefulWidget {
 class _FavoritesListWidgetState extends State<FavoritesListWidget> {
   final _books = [
     Books(
+        id: 1,
         imageName: 'assets/images/p_luca_21670_3c13c611.jpeg',
         title: 'Anna Karenina by Leo Tolstoy',
         bookInfo: 'Book info',
         description:
             'Set in 19th-century Russia, this novel revolves around the life of Anna Karenina, a high-society woman who, dissatisfied with her loveless marriage, embarks on a passionate affair with a charming officer named Count Vronsky. This scandalous affair leads to her social downfall, while parallel to this, the novel also explores the rural life and struggles of Levin, a landowner who seeks the meaning of life and true happiness. The book explores themes such as love, marriage, fidelity, societal norms, and the human quest for happiness.'),
     Books(
+        id: 2,
         imageName: 'assets/images/p_luca_21670_3c13c611.jpeg',
         title: 'Madame Bovary by Gustave Flaubert',
         bookInfo: 'Book info',
         description:
             'Madame Bovary is a tragic novel about a young woman, Emma Bovary, who is married to a dull, but kind-hearted doctor. Dissatisfied with her life, she embarks on a series of extramarital affairs and indulges in a luxurious lifestyle in an attempt to escape the banalities and emptiness of provincial life. Her desire for passion and excitement leads her down a path of financial ruin and despair, ultimately resulting in a tragic end.'),
     Books(
+        id: 3,
         imageName: 'assets/images/p_luca_21670_3c13c611.jpeg',
         title: 'War and Peace by Leo Tolstoy',
         bookInfo: 'Book info',
         description:
             'Set in the backdrop of the Napoleonic era, the novel presents a panorama of Russian society and its descent into the chaos of war. It follows the interconnected lives of five aristocratic families, their struggles, romances, and personal journeys through the tumultuous period of history. The narrative explores themes of love, war, and the meaning of life, as it weaves together historical events with the personal stories of its characters.'),
     Books(
+        id: 4,
         imageName: 'assets/images/p_luca_21670_3c13c611.jpeg',
         title: 'The Great Gatsby by F. Scott Fitzgerald',
         bookInfo: 'Book info',
         description:
             "Set in the summer of 1922, the novel follows the life of a young and mysterious millionaire, his extravagant lifestyle in Long Island, and his obsessive love for a beautiful former debutante. As the story unfolds, the millionaire/'s dark secrets and the corrupt reality of the American dream during the Jazz Age are revealed. The narrative is a critique of the hedonistic excess and moral decay of the era, ultimately leading to tragic consequences."),
     Books(
+        id: 5,
         imageName: 'assets/images/p_luca_21670_3c13c611.jpeg',
         title: 'Lolita by Vladimir Nabokov',
         bookInfo: 'Book info',
@@ -81,6 +89,11 @@ class _FavoritesListWidgetState extends State<FavoritesListWidget> {
     _searchController.addListener(() {
       _searchBooks();
     });
+  }
+
+  void _onBookTap(int index) {
+    final id = _books[index].id;
+    Get.toNamed('/readAndBuyPage', arguments: id);
   }
 
   @override
@@ -150,9 +163,7 @@ class _FavoritesListWidgetState extends State<FavoritesListWidget> {
                       color: Colors.transparent,
                       child: InkWell(
                         borderRadius: BorderRadius.circular(10),
-                        onTap: () {
-                          Get.toNamed('/readAndBuyPage');
-                        },
+                        onTap: () => _onBookTap(index),
                       ),
                     )
                   ],
