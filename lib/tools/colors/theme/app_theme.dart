82c97fee567bf6a57ddaac01069ff807a84216b0
John Doe <ownworkplace01@gmail.com>
2024-11-14 22:09:21 +0500

    theme data not all ended

--- lib/tools/colors/theme/app_theme.dart
+++ lib/tools/colors/theme/app_theme.dart
@@ -1,87 +1,53 @@
-// // lib/theme/app_theme.dart
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:online_library/tools/colors/onlinelibrary_colors.dart';
-
-// class AppTheme {
-//   static ThemeData get theme {
-//     return ThemeData(
-//       inputDecorationTheme: InputDecorationTheme(
-//         hintStyle: TextStyle(color: AppColors.mainColor),
-//         labelStyle: TextStyle(color: AppColors.mainColor),
-//         border: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(20),
-//           borderSide: BorderSide(color: AppColors.mainColor),
-//         ),
-//         enabledBorder: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(20),
-//           borderSide: BorderSide(color: AppColors.mainColor),
-//         ),
-//         focusedBorder: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(20),
-//           borderSide: BorderSide(color: AppColors.mainColor),
-//         ),
-//       ),
-//       scaffoldBackgroundColor: AppColors.mainWhite,
-//       primaryColor: AppColors.mainColorWhite,
-//       primaryColorLight: AppColors.mainColor,
-//       primaryColorDark: AppColors.mainWhite,
-//       textTheme: GoogleFonts.kanitTextTheme().apply(
-//           bodyColor: AppColors.mainColor,
-//           displayColor: AppColors.mainWhite,
-//           decorationColor: AppColors.mainColor),
-//       shadowColor: Colors.grey.withOpacity(0.2),
-//       appBarTheme: AppBarTheme(
-//         backgroundColor: AppColors.mainWhite,
-//         iconTheme: IconThemeData(
-//           color: AppColors.mainColor,
-//         ),
-//         elevation: 2.0,
-//       ),
-//       bottomNavigationBarTheme: BottomNavigationBarThemeData(
-//         backgroundColor: AppColors.mainWhite,
-//         selectedItemColor: AppColors.mainColor,
-//         unselectedItemColor: AppColors.mainColor,
-//       ),
-//       tabBarTheme: TabBarTheme(
-//           labelColor: AppColors.mainColor,
-//           unselectedLabelColor: AppColors.mainWhite,
-//           indicatorColor: AppColors.mainColorWhite),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:online_library/tools/colors/onlinelibrary_colors.dart';
 
 class AppThemes {
   static final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    primaryColor: const Color.fromARGB(255, 236, 236, 236),
-    cardColor: const Color.fromARGB(255, 255, 255, 255),
+    primaryColor: AppColors.mainColorWhite,
+    scaffoldBackgroundColor: AppColors.mainWhite,
+    primaryColorLight: AppColors.mainColor,
+    primaryColorDark: AppColors.mainWhite,
     iconTheme: const IconThemeData(color: Color.fromARGB(255, 50, 161, 252)),
-    textTheme: GoogleFonts.jostTextTheme(
-      ThemeData.light().textTheme.copyWith(
-          bodyLarge: GoogleFonts.jost(
-            color: const Color.fromRGBO(42, 43, 42, 1),
-            fontSize: 28.0,
-            fontWeight: FontWeight.bold,
-          ),
-          bodyMedium: GoogleFonts.jost(
-            color: const Color.fromRGBO(42, 43, 42, 1),
-            fontSize: 16.0,
-            fontWeight: FontWeight.normal,
-          ),
-          bodySmall: GoogleFonts.jost(
-              color: const Color.fromARGB(221, 0, 153, 255),
-              fontSize: 16.0,
-              fontWeight: FontWeight.normal)),
-    ),
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Color.fromARGB(255, 236, 236, 236),
-      foregroundColor: Color.fromRGBO(255, 255, 255, 1),
-    ),
-  );
+    textTheme: GoogleFonts.kanitTextTheme().apply(
+          bodyColor: AppColors.mainColor,
+          displayColor: AppColors.mainWhite,
+          decorationColor: AppColors.mainColor,),
+    
+    appBarTheme: AppBarTheme(
+        backgroundColor: AppColors.mainWhite,
+        iconTheme: IconThemeData(
+          color: AppColors.mainColor,
+        ),
+        elevation: 2.0,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        hintStyle: TextStyle(color: AppColors.mainColor),
+        labelStyle: TextStyle(color: AppColors.mainColor),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(color: AppColors.mainColor),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(color: AppColors.mainColor),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(color: AppColors.mainColor),
+        ),
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: AppColors.mainWhite,
+        selectedItemColor: AppColors.mainColor,
+        unselectedItemColor: AppColors.mainColor,
+      ),
+      tabBarTheme: TabBarTheme(
+          labelColor: AppColors.mainColor,
+          unselectedLabelColor: AppColors.mainWhite,
+          indicatorColor: AppColors.mainColorWhite),
+    );
 
   static final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
